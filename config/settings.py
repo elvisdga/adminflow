52f3ad994ded4e92c31cd609c9ce61e16017e92e
Elvis Aguirre <elvisgaitan@gmail.com>
2026-05-16 15:53:50 -0500

    feat: add CSRF_TRUSTED_ORIGINS for production

--- config/settings.py
+++ config/settings.py
@@ -9,6 +9,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config('SECRET_KEY')
 DEBUG = config('DEBUG', default=False, cast=bool)
 ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
+CSRF_TRUSTED_ORIGINS = config('CSRF_TRUSTED_ORIGINS', cast=Csv(), default='')
 
 # ── Aplicaciones instaladas ──────────────────────────────────
 INSTALLED_APPS = [
